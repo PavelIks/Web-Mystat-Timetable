bedab7d43e04c901d3c524dec8dd6852c3176d4c
Pavlo Ocheretianyi <pavel_ocheretyany_01@mail.ua>
2020-07-24 00:18:37 +0300

    Updated-2.

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 window.onload = function ()
 {
-    document.body.classList.add('loaded_hiding');
-    window.setTimeout(function ()
-    {
-      document.body.classList.add('loaded');
-      document.body.classList.remove('loaded_hiding');
-    }, 500);
-  }
+  document.body.classList.add('loaded_hiding');
+  window.setTimeout(function ()
+  {
+    document.body.classList.add('loaded');
+    document.body.classList.remove('loaded_hiding');
+  }, 500);
+}
